fcd09fe4d05cc746153bac83b56aa58c7a3e65bb
yosephNative <safaridagm@gmail.com>
2025-09-27 20:12:39 +0300

    not complete

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,8 +6,11 @@ import Employee from "../model/employee.js";
 
 export async function connectDB(){
    console.log('inside db function');
+   
 try{
   const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/FullstackDB';
+  console.log('MONGODB_URI environment variable:', process.env.MONGODB_URI ? 'SET' : 'NOT SET');
+  console.log('Using MongoDB URI:', mongoURI);
   const connn = await mongoose.connect(mongoURI);
   console.log('database connected successfully');
 
